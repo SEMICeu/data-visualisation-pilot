748a88ecf601f3cd6c67e7d87f64fc62bc448ced
brule.daniel@gmail.com <brule.daniel@gmail.com>
2017-06-28 15:51:16 +0200

    fixing multilingual issues

--- TED/datasets/languages.js
+++ TED/datasets/languages.js
@@ -2,5 +2,9 @@ var texts = {
 'en-US': {hover:"Hover over a state",
         nTenders:"Number of tenders: ",
         tList:"List of tenders",
-        noTenders:"No tenders"}
+        noTenders:"No tenders"},
+'fr': {hover:"Sélectionner un pays",
+        nTenders:"Nombre d'offres: ",
+        tList:"Liste d'offres",
+        noTenders:"Aucune offre"}
 }
